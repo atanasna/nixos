a669d0455c9fa4f0f4bedee9661085f3d66fbe1e
Atanas Atanasov <atanas.atanasov@ampeco.global>
2025-07-28 09:59:01 +0300

    test 3

--- hosts/atanas-n1/configuration.nix
+++ hosts/atanas-n1/configuration.nix
@@ -2,29 +2,33 @@
 {
 
   mods = {
-    universal.networking = {
-      interfaces = [
-        {
-          name = "enp0s1";
-          addresses = [
-            { 
-              address = "192.168.66.11"; 
-              prefixLength = 24; 
-            }
-          ];
-        }
-      ];
-      gateway = "192.168.66.1";
-      nameservers = [ "8.8.8.8" "4.4.4.4" ];
+    universal = {
+      networking = {
+        interfaces = [
+          {
+            name = "enp0s1";
+            addresses = [
+              { 
+                address = "192.168.66.11"; 
+                prefixLength = 24; 
+              }
+            ];
+          }
+        ];
+        gateway = "192.168.66.1";
+        nameservers = [ "8.8.8.8" "4.4.4.4" ];
+      };
+      system = {
+        hostname = "atanas-n1";
+        arch = "aarch64-linux";
+      };
     };
-    general = {
-      hostname = "atanas-n1";
-      arch = "aarch64-linux";
-    };
-    roles.lab.worker = {
-      k3s = {
-        init = true;
-        token = builtins.readFile ../../secrets/k3s_token;
+    roles = {
+      lab.worker = {
+        k3s = {
+          init = true;
+          token = builtins.readFile ../../secrets/k3s_token;
+        };
       };
     };
   };
